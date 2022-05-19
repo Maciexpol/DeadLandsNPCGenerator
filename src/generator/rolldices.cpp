9de6c89a7aaaa94fbab116c8b0da96d25cab2eb8
Maciexpol <empieniazek@student.agh.edu.pl>
2022-05-19 16:23:17 +0200

    Init rolldices namespace

---
+++ src/generator/rolldices.cpp
@@ -0,0 +1,6 @@
+#include "rolldices.h"
+
+RollDices::RollDices()
+{
+
+}
---
+++ src/generator/rolldices.h
@@ -0,0 +1,11 @@
+#ifndef ROLLDICES_H
+#define ROLLDICES_H
+
+
+class RollDices
+{
+public:
+    RollDices();
+};
+
+#endif // ROLLDICES_H
